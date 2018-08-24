a9ba5bd5648f2bac465c72df5191eadb602d7f3c
정재엽 <37529449+skyla15@users.noreply.github.com>
2018-08-24 20:44:54 +0900

    Update reg.php

--- Asyntask/reg.php
+++ Asyntask/reg.php
@@ -14,13 +14,19 @@
 	$pw = $_POST['pw'];
 	$name = $_POST['name'];
 
+	$sql = "SELECT id FROM users WHERE id = '$id'";//중복체크
+	$result = $con->query($sql);
+	if($result->num_rows > 0){
+		echo "exist";
+	}
+	else{
+		$sql = "INSERT INTO user (id, pw, name) VALUES ('$id', '$pw', '$name')";
 
-	$sql = "INSERT INTO user (id, pw, name) VALUES ('$id', '$pw', '$name')";
-
-	if($con->query($sql) == TRUE){
-		echo "success";
-	}else{
-		echo "error";
+		if($con->query($sql) == TRUE){
+			echo "success";
+		}else{
+			echo "error";
+		}
 	}
 
 	$con->close();
